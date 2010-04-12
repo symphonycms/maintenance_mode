3ded118305030bf3b5bdf58c7405a9a96d478d98
pointybeard <alistair@pointybeard.com>
2010-04-12 17:10:20 +1000

    Added extensions type and changed references 'Preferences' references to 'Settings'

--- extension.driver.php
+++ extension.driver.php
@@ -6,6 +6,7 @@
 			return array('name' => 'Maintenance Mode',
 						 'version' => '1.2',
 						 'release-date' => '2010-02-02',
+						 'type' => array('Core'),
 						 'author' => array('name' => 'Alistair Kearney',
 										   'website' => 'http://pointybeard.com',
 										   'email' => 'alistair@pointybeard.com')
@@ -15,19 +16,19 @@
 		public function getSubscribedDelegates(){
 			return array(
 						array(
-							'page' => '/system/preferences/',
-							'delegate' => 'AddCustomPreferenceFieldsets',
+							'page' => '/system/settings/',
+							'delegate' => 'AddCustomToolFieldsets',
 							'callback' => 'appendPreferences'
 						),
 						
 						array(
-							'page' => '/system/preferences/',
+							'page' => '/system/settings/',
 							'delegate' => 'Save',
 							'callback' => '__SavePreferences'
 						),							
 						
 						array(
-							'page' => '/system/preferences/',
+							'page' => '/system/settings/',
 							'delegate' => 'CustomActions',
 							'callback' => '__toggleMaintenanceMode'
 						),
@@ -56,10 +57,10 @@
 		public function __toggleMaintenanceMode($context){
 			
 			if($_REQUEST['action'] == 'toggle-maintenance-mode'){			
-				$value = ($this->_Parent->Configuration->get('enabled', 'maintenance_mode') == 'no' ? 'yes' : 'no');					
-				$this->_Parent->Configuration->set('enabled', $value, 'maintenance_mode');
-				$this->_Parent->saveConfig();
-				redirect((isset($_REQUEST['redirect']) ? URL . '/symphony' . $_REQUEST['redirect'] : $this->_Parent->getCurrentPageURL() . '/'));
+				$value = (Symphony::Configuration()->get('enabled', 'maintenance_mode') == 'no' ? 'yes' : 'no');					
+				Symphony::Configuration()->set('enabled', $value, 'maintenance_mode');
+				Symphony::Parent()->saveConfig();
+				redirect((isset($_REQUEST['redirect']) ? ADMIN_URL . '' . $_REQUEST['redirect'] : Symphony::Parent()->getCurrentPageURL() . '/'));
 			}
 			
 		}
@@ -68,27 +69,27 @@
 			
 			if(!is_null($context['alert'])) return;
 			
-			if($this->_Parent->Configuration->get('enabled', 'maintenance_mode') == 'yes'){
-				Administration::instance()->Page->pageAlert(__('This site is currently in maintenance mode.') . ' <a href="' . URL . '/symphony/system/preferences/?action=toggle-maintenance-mode&amp;redirect=' . getCurrentPage() . '">' . __('Restore?') . '</a>', Alert::NOTICE);
+			if(Symphony::Configuration()->get('enabled', 'maintenance_mode') == 'yes'){
+				Administration::instance()->Page->pageAlert(__('This site is currently in maintenance mode.') . ' <a href="' . ADMIN_URL . '/system/preferences/?action=toggle-maintenance-mode&amp;redirect=' . getCurrentPage() . '">' . __('Restore?') . '</a>', Alert::NOTICE);
 			}
 		}
 		
 		public function __addParam($context){
-			$context['params']['site-mode'] = ($this->_Parent->Configuration->get('enabled', 'maintenance_mode') == 'yes' ? 'maintenance' : 'live'); 
+			$context['params']['site-mode'] = (Symphony::Configuration()->get('enabled', 'maintenance_mode') == 'yes' ? 'maintenance' : 'live'); 
 		}
 		
 		public function __checkForMaintenanceMode($context){
 			
-			if(!$this->_Parent->isLoggedIn() && $this->_Parent->Configuration->get('enabled', 'maintenance_mode') == 'yes'){
+			if(!Symphony::Parent()->isLoggedIn() && Symphony::Configuration()->get('enabled', 'maintenance_mode') == 'yes'){
 				
-				$context['row'] = $this->_Parent->Database->fetchRow(0, "
+				$context['row'] = Symphony::Database()->fetchRow(0, "
 											SELECT `tbl_pages`.* FROM `tbl_pages`, `tbl_pages_types` 
 											WHERE `tbl_pages_types`.page_id = `tbl_pages`.id 
 											AND tbl_pages_types.`type` = 'maintenance' 
 											LIMIT 1");
 			
 				if(empty($context['row'])){
-					$this->_Parent->customError(E_USER_ERROR, __('Website Offline'), __('This site is currently in maintenance. Please check back at a later date.'), false, true);
+					Symphony::Parent()->customError(E_USER_ERROR, __('Website Offline'), __('This site is currently in maintenance. Please check back at a later date.'), false, true);
 				}
 				
 			}
@@ -113,7 +114,7 @@
 			
 			$label = Widget::Label();
 			$input = Widget::Input('settings[maintenance_mode][enabled]', 'yes', 'checkbox');
-			if($this->_Parent->Configuration->get('enabled', 'maintenance_mode') == 'yes') $input->setAttribute('checked', 'checked');
+			if(Symphony::Configuration()->get('enabled', 'maintenance_mode') == 'yes') $input->setAttribute('checked', 'checked');
 			$label->setValue($input->generate() . ' ' . __('Enable maintenance mode'));
 			$group->appendChild($label);
 						
