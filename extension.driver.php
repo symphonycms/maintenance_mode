99f4c7d78de6500229d6cf1c41b759f4e70f0a79
Miguel Payet <miguelpayet@gmail.com>
2015-03-19 13:21:58 -0500

    Quick fix for empty useragent whitelist
    Quick fix for a warning on call to implode() when an empty useragent whitelist is read.
    Happened on a 2.6.0 site that has just been upgraded from 2.5.3.
    
    The warning is:
    Symphony Warning: implode(): Invalid arguments passed
    An error occurred in /Users/miguel/Sites/susanwagner/extensions/maintenance_mode/extension.driver.php around line 90

--- extension.driver.php
+++ extension.driver.php
@@ -87,7 +87,9 @@ Class extension_maintenance_mode extends Extension
 
         // Useragent White list
         $label = Widget::Label(__('Useragent Whitelist'));
-        $useragent = implode("\r\n",json_decode(Symphony::Configuration()->get('useragent_whitelist', 'maintenance_mode')));
+        $whitelist = Symphony::Configuration()->get('useragent_whitelist', 'maintenance_mode');
+        if ($whitelist) 
+            $useragent = implode("\r\n",json_decode(Symphony::Configuration()->get('useragent_whitelist', 'maintenance_mode')));
         $label->appendChild(Widget::Textarea('settings[maintenance_mode][useragent_whitelist]', 5, 50, $useragent));
         $group->appendChild($label);
 
